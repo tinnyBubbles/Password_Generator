9ce612fb64ac15f173af52c830bb62cdc4424c86
Blackfoot50115 <49222054+blackfoot50115@users.noreply.github.com>
2019-05-02 09:50:40 -0400

    Created function to generate a random hex of specified byte length.

---
+++ main.py
@@ -0,0 +1,29 @@
+from random import getrandbits
+
+# Ask user to enter program or exit
+enter_Program = input("Enter 'Y' to generate a password or press any key to exit")
+enter_Program = enter_Program.lower()
+
+
+# Functions
+def gen_rand_password(b_length):
+    """Generates a random number of a specified bit length then converts to hex.
+       Returns an int.
+    """
+    random_int = getrandbits(b_length)
+    return random_int
+
+
+def salt(rand_string):
+    """inserts random symbols into specified random string.
+       returns string
+    """
+
+
+#  Get user input/ bit length of password/ file path to store password
+
+if enter_Program == "y":
+    print(gen_rand_password(16))
+
+else:
+    exit()
