b3598d6602185b9e0158858dead41ebfc0c4fdb0
Blackfoot50115 <49222054+blackfoot50115@users.noreply.github.com>
2019-05-04 23:35:10 -0400

    Designed class BuildGui

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import gen_rand_password as gen
 from tkinter import *
 
 
+password = "1 "
+pass_length = 15
+
+
 def exit():
     """
     Exits the program
@@ -10,30 +14,43 @@ def exit():
     return True
 
 
-def generate_password(length):
-    '''
+'''def generate_password():
+    
     Returns a randomly generated string of int length
+    
+
+    # return gen.gen_rand_password(length)
+    global password
+    password = gen.gen_rand_password(pass_length)
     '''
 
-    return gen.gen_rand_password(length)
 
+class BuildGui:
 
-def build_gui():
-    window = Tk()
-    window.geometry('1000x100')
-    window.title("Password Generator")
+    def __init__(self, char_length):
+        self.password_length = char_length
 
-    lbl = Label(window, text='password')
+        self.window = Tk()
+        self.window.geometry('1000x100')
+        self.window.title("Password Generator")
 
-    btn_generate = Button(window, text='Generate Password')
-    lbl.pack()
-    btn_generate.pack()
+        self.lbl = Label(self.window, text=password)
+        self.btn_generate = Button(self.window, text='Generate Password', command=self.generate_password())
 
-    return window
+        self.lbl.pack()
+        self.btn_generate.pack()
+        self.window.mainloop()
 
+    def generate_password(self):
+        '''
+        Returns a randomly generated string of int length
+        '''
+
+        # return gen.gen_rand_password(length)
+        global password
+        password = gen.gen_rand_password(self.password_length)
+        self.lbl.configure(text=password)
 
 
 if __name__ == '__main__':
-
-    gui = build_gui()
-    gui.mainloop()
+    gui = BuildGui(pass_length)
