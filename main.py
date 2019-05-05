55ee473fd491c3c7820456f2468b6348e39792dd
Blackfoot50115 <49222054+blackfoot50115@users.noreply.github.com>
2019-05-04 21:06:50 -0400

    Create a module that generates a random password of x_length characters.
    Deleted gen_rand_password function from main.py. Instead, main.py calls it from gen_rand_password.py

--- main.py
+++ main.py
@@ -1,29 +1,13 @@
-from random import getrandbits
+import gen_rand_password as gen
+
 
 # Ask user to enter program or exit
 enter_Program = input("Enter 'Y' to generate a password or press any key to exit")
 enter_Program = enter_Program.lower()
 
 
-# Functions
-def gen_rand_password(b_length):
-    """Generates a random number of a specified bit length then converts to hex.
-       Returns an hex.
-    """
-    random_hex = hex(getrandbits(b_length))
-    return random_hex
-
-
-def salt(rand_string):
-    """inserts random symbols into specified random string.
-       returns string
-    """
-
-
-#  Get user input/ bit length of password/ file path to store password
-
 if enter_Program == "y":
-    print(gen_rand_password(64))
+    print(gen.gen_rand_password(15))
 
 else:
     exit()
